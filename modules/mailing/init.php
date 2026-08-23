5acfc7cd5da540b5d5995d89122ed603e349efd0
Iván Andrés Arriola Turné <83203492+ivan1arriola@users.noreply.github.com>
2026-08-23 03:39:48 -0300

    refactor(mailing): register homepage adapter

--- modules/mailing/init.php
+++ modules/mailing/init.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Módulo de Mailing - FLACSO Uruguay
- * Suscripción de contactos a listas de Mailjet
+ * Suscripción de contactos a listas de Mailjet.
  */
 
 if (!defined('ABSPATH')) {
@@ -11,16 +11,15 @@ if (!defined('ABSPATH')) {
 if (!defined('FLACSO_MAILING_MODULE_PATH')) {
     define('FLACSO_MAILING_MODULE_PATH', __DIR__ . '/');
 }
-
 if (!defined('FLACSO_MAILING_MODULE_URL')) {
     define('FLACSO_MAILING_MODULE_URL', plugin_dir_url(__FILE__));
 }
-
 if (!defined('FLACSO_MAILING_MODULE_VERSION')) {
     define('FLACSO_MAILING_MODULE_VERSION', FLACSO_URUGUAY_VERSION);
 }
 
 flacso_safe_require('modules/mailing/includes/class-flacso-mailing-subscription.php');
+flacso_safe_require('modules/mailing/includes/homepage.php');
 
 if (class_exists('Flacso_Mailing_Subscription')) {
     Flacso_Mailing_Subscription::init();
