e4deda5ccc09d8db6dc45ffc5b3d637cc33a35f5
Iván Andrés Arriola Turné <83203492+ivan1arriola@users.noreply.github.com>
2026-08-23 03:42:55 -0300

    refactor(eventos): fail fast on required includes

--- modules/eventos/init.php
+++ modules/eventos/init.php
@@ -7,11 +7,9 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-flacso_safe_require('modules/eventos/includes/class-cpt-eventos-manager.php');
-flacso_safe_require('modules/eventos/includes/homepage.php');
+flacso_require('modules/eventos/includes/class-cpt-eventos-manager.php');
+flacso_require('modules/eventos/includes/homepage.php');
 
 add_action('init', static function (): void {
-    if (class_exists('CPT_Eventos_Manager')) {
-        new CPT_Eventos_Manager();
-    }
+    new CPT_Eventos_Manager();
 }, 5);
