4518881ebcf84eaa13b35658f55d140469eb2b5e
Iván Andrés Arriola Turné <83203492+ivan1arriola@users.noreply.github.com>
2026-09-04 16:43:18 -0300

    feat(carta): cargar editor de contacto de oferta

--- modules/oferta-academica/init.php
+++ modules/oferta-academica/init.php
@@ -17,6 +17,7 @@ flacso_safe_require('modules/oferta-academica/includes/class-oferta-admin-table-
 flacso_safe_require('modules/oferta-academica/includes/class-cpt-programa-academico.php');
 flacso_safe_require('modules/oferta-academica/includes/class-oferta-academica.php');
 flacso_safe_require('modules/oferta-academica/includes/class-oferta-admin-fields.php');
+flacso_safe_require('modules/oferta-academica/includes/class-offer-carta-contact-admin.php');
 flacso_safe_require('modules/oferta-academica/includes/class-cohorte.php');
 flacso_safe_require('modules/oferta-academica/includes/class-academic-document-source-admin.php');
 // No cargar el editor legado: en algunos runtimes quedó una definición antigua
@@ -50,6 +51,7 @@ add_action('init', static function (): void {
 }, 5);
 
 FLACSO_Oferta_Admin_Fields::init();
+FLACSO_Offer_Carta_Contact_Admin::init();
 FLACSO_Academic_Document_Source_Admin::init();
 FLACSO_Academic_Team_Editor::init();
 Tabla_Precio_Schema::init();
